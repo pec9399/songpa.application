335e92657714a412f4f753aff7668f6942836269
hayeon <qorgkdus12@naver.com>
2022-09-05 16:20:47 +0900

    [Edit] Connect server & web

--- web/src/modules/index.jsx
+++ web/src/modules/index.jsx
@@ -1,10 +1,13 @@
-import {combineReducers} from 'redux';
-import {all} from 'redux-saga/effects';
+import { combineReducers } from 'redux';
+import { all } from 'redux-saga/effects';
 import UserReducer from './User/reducer'
+import MainReducer from './Main/reducer'
 import UserSagaListener from './User/sagas';
+import MainSagaListener from './Main/sagas';
 
 const rootReducer = combineReducers({
   user: UserReducer,
+  main: MainReducer
 });
 
 export default rootReducer;
@@ -12,5 +15,6 @@ export default rootReducer;
 export function* rootSaga() {
   yield all([
     UserSagaListener(),
+    MainSagaListener()
   ]);
 }
